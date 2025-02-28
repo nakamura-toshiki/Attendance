c4e1ccbf0875113434c14f4a52e78aa62535f35a
nakamura toshiki <toshibo.1102@outlook.jp>
2025-02-28 19:54:39 +0900

    src再コミット

--- src/resources/views/admin/list.blade.php
+++ src/resources/views/admin/list.blade.php
@@ -9,17 +9,17 @@
     <h2 class="list-heading">勤怠一覧</h2>
     <div class="list-month">
         <div class="previous">
-            <img class="previous-img" src="{{ asset('storage/arrow.png') }}" alt="矢印">
+            <img class="previous-img" src="{{ asset('storage/images/arrow.png') }}" alt="矢印">
             <a class="previous-link" href="?date={{ $previousDay }}">前日</a>
         </div>
         <div class="current">
-            <img class="current-img" src="{{ asset('storage/calendar.png') }}" alt="カレンダー">
+            <img class="current-img" src="{{ asset('storage/images/calendar.png') }}" alt="カレンダー">
             <span class="current-date">{{ $current->isoFormat('YYYY/MM/DD') }}</span>
         </div>
         @if($current->format('Y-m-d') < \Carbon\Carbon::today()->format('Y-m-d'))
             <div class="next">
                 <a class="next-link" href="?date={{ $nextDay }}">翌日</a>
-                <img class="next-img" src="{{ asset('storage/arrow.png') }}" alt="矢印">
+                <img class="next-img" src="{{ asset('storage/images/arrow.png') }}" alt="矢印">
             </div>
         @else
             <span class="empty"></span>
--- src/resources/views/admin/person.blade.php
+++ src/resources/views/admin/person.blade.php
@@ -9,17 +9,17 @@
     <h2 class="list-heading">{{$user->name}}さんの勤怠一覧</h2>
     <div class="list-month">
         <div class="previous">
-            <img class="previous-img" src="{{ asset('storage/arrow.png') }}" alt="矢印">
+            <img class="previous-img" src="{{ asset('storage/images/arrow.png') }}" alt="矢印">
             <a class="previous-link" href="?date={{ $previousMonth }}">前月</a>
         </div>
         <div class="current">
-            <img class="current-img" src="{{ asset('storage/calendar.png') }}" alt="カレンダー">
+            <img class="current-img" src="{{ asset('storage/images/calendar.png') }}" alt="カレンダー">
             <span class="current-date">{{ $current->isoFormat('YYYY/MM') }}</span>
         </div>
         @if($current->format('Y-m') < \Carbon\Carbon::today()->format('Y-m'))
             <div class="next">
                 <a class="next-link" href="?date={{ $nextMonth }}">翌月</a>
-                <img class="next-img" src="{{ asset('storage/arrow.png') }}" alt="矢印">
+                <img class="next-img" src="{{ asset('storage/images/arrow.png') }}" alt="矢印">
             </div>
         @else
             <span class="empty"></span>
